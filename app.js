082564a8c9b1397f19f148fac164dd91ad824040
LeviUdoh-17 <leviudoh17@gmail.com>
2024-12-26 22:48:40 +0100

    Add typing effect to intro text and style adjustments for better visibility

--- app.js
+++ app.js
@@ -16,5 +16,29 @@ document.querySelector('.nav-link[href="#intro"]').addEventListener('click', fun
     }, 3000); // Blink effect lasts 3 seconds
 });
 
+// Typing effect for #typing-effect
+const texts = ["FRONT-END WEB DEVELOPER", "EMBEDDED SYSTEMS DEVELOPER"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById('typing-effect').innerHTML = `<span class="typing">${letter}</span>`;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+        setTimeout(type, 2000); // Wait for 2 seconds before starting the next text
+    } else {
+        setTimeout(type, 100); // Typing speed
+    }
+})();
+
 // Set the current year in the footer
 document.getElementById("copyright").innerHTML = `Copyright &copy ${new Date().getFullYear()}; Levi Udoh. All rights reserved`;
